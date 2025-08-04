46d56393253f2bb6ddbae4681d3f0b06191cdec0
DanielaP <danielaparedes22@gmail.com>
2025-08-04 12:24:55 -0500

    first commit

---
+++ EJER/ej2.js
@@ -0,0 +1,16 @@
+let ciudad="popayan"
+
+function obtenerciudad(){
+    let ciudad="cali"
+    console.log (ciudad)
+    ciudad="Neivayork"
+    console.log (ciudad)
+    return ciudad
+}
+if (true){
+    let ciudad="asdadasd"
+}
+
+console.log (ciudad)
+console.log (obtenerciudad())
+
